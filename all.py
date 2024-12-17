d6050743e51a2e32b9de86bc3f86611e12f099c1
Food-L <fudlelmachfudh@gmail.com>
2024-12-18 00:23:12 +0700

    Create all.py
    Learn Python and save in github

---
+++ all.py
@@ -0,0 +1,96 @@
+
+# jumlah = 5
+# for i in range(1, 11):
+#     jumlah += i
+# print("Jumlah 1 hingga 10 adalah : ", jumlah)
+# _________________________________________________
+
+# jumlah = 0
+# i = 1
+# while
+# _________________________________________________
+
+# a = (input("masukkan a : "))
+# b = (input("masukkan b : "))
+# c = (input("masukkan c : "))
+# ## c < a > b
+# ## if a > b and a > c:
+# if c < a > b:
+#     p_gede = a
+# elif b > c:
+#     p_gede = b
+# else:
+#     p_gede = c
+
+# print("Paling besar adalah : ", p_gede)
+
+# ____________________________________________
+
+# list_1 = [10, 70, 20]
+# # list dengan deklarasi element secara vertikal
+# list_2 = [
+# 'ab',
+# 'cd',
+# 'hi',
+# 'ca'
+# ]
+# # list dengan element berisi bermacam-macam tipe data
+# list_3 = [3.14, 'hello python', True, False]
+# # list kosong
+# list_4 = []
+# _________________________________________
+
+# list_1 = [10, 70, 20]
+# for e in list_1:
+#     print("elem:", e)
+
+
+# for d in range (3, -4, -1):
+#     print("index:", d)
+# __________________________________________
+
+# pesan1 = '''ini tulisan pertama'''
+# pesan2 = '''ini tulisan kedua'''
+# pesan3 = '''ini tulisan ketiga'''
+
+# print(pesan1, pesan2, pesan3)
+# _________________________________________
+
+# List Type Variable 
+# # list with int as element's data type
+# list_1 = [2, 4, 8, 16]
+# # list with str as element's data type
+# list_2 = ["grayson", "jason", "tim", "damian"]
+# # list with various data type in the element
+# list_3 = [24, False, "Hello Python"]
+
+# print(list_1, list_2)
+# _________________________________________
+
+# # name_1 = str(input('Masukkan Nama :'))
+# name_1 = 'Rowen Adkinson'
+# name_2 = 'Jack Sparrow'
+# name_3 = "Narto Saripuden"
+
+# # some_name = f'{name_1} , {name_2}'
+# some_name = '{} {}'.format(name_1, name_3)
+
+# # print(name_1.lower())
+# # print(name_1.upper())
+# # print(name_1.title())
+# # print(f'Menampilkan 2 nama dangan tambahan (f) : {name_3}, {name_1}!')
+# print(some_name)
+# # full_name = "{} {}".format(first_name, last_name)
+# ________________________________________
+
+# Quote Text
+
+text = 'Tulisan rstript '
+# text.rstrip()
+name = str('Grim Hustle')
+name.rstrip()
+quote = str('"Be Honorable!"')
+# print(name, Quote)
+print(name,".say :", quote)
+print(text)
+
